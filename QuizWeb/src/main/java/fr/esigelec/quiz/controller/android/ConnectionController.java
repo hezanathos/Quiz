e3b4ae09a8c5aa01b5fc70d3c2fefa73c88787f9
Edouard PETIT <edouard.petit.16@gmail.com>
2017-01-04 17:12:47 +0100

    Edouard

--- QuizWeb/src/main/java/fr/esigelec/quiz/controller/android/ConnectionController.java
+++ QuizWeb/src/main/java/fr/esigelec/quiz/controller/android/ConnectionController.java
@@ -2,4 +2,6 @@ package fr.esigelec.quiz.controller.android;
 
 public class ConnectionController {
 
+
+
 }
--- QuizWeb/src/main/java/fr/esigelec/quiz/controller/web/QuizController.java
+++ QuizWeb/src/main/java/fr/esigelec/quiz/controller/web/QuizController.java
@@ -1,4 +1,4 @@
-package fr.esigelec.quiz.controller;
+package fr.esigelec.quiz.controller.web;
 
 /**
  * Created by Edouard on 04/01/2017.
