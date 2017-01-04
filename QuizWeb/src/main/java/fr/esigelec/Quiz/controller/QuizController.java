cf9e7d83101d985eb6bf57fb7c689489d841ee81
Starpisaurus <edouard.petit.16@gmail.com>
2017-01-04 16:30:59 +0100

    Ajout des controllers

---
+++ QuizWeb/src/main/java/fr/esigelec/Quiz/controller/QuizController.java
@@ -0,0 +1,7 @@
+package fr.esigelec.Quiz.controller;
+
+/**
+ * Created by Edouard on 04/01/2017.
+ */
+public class QuizController {
+}
---
+++ QuizWeb/src/main/java/fr/esigelec/Quiz/controller/UserController.java
@@ -0,0 +1,9 @@
+package fr.esigelec.Quiz.controller;
+
+/**
+ * Created by Edouard on 04/01/2017.
+ */
+public class UserController {
+
+
+}
