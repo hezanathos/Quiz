5016d856939c64de1e4bb7735340f36b903ca092
Guillaume PILLET <guillaume.pillet@amadeus.com>
2017-01-04 16:41:49 +0100

    Android commit 0

--- QuizAndroid/app/src/main/java/fr/esigelec/quiz/QuizActivity.java
+++ QuizAndroid/app/src/main/java/fr/esigelec/quiz/QuizActivity.java
@@ -1,11 +1,36 @@
 package fr.esigelec.quiz;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.os.Build;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+    private TextView qNumber;
+    private TextView qQuestion;
+    private String aNumber;
+    private String aQuestion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +38,108 @@ public class QuizActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        connectWebSocket();
         setContentView(R.layout.activity_quiz);
+        List<Fragment> fList = new ArrayList<>();
+        fList.add(new ResponseFragment());
+        qNumber = (TextView) findViewById(R.id.qNumber);
+        qQuestion = (TextView) findViewById(R.id.qQuestion);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager(), fList);
+        mPager.setAdapter(mPagerAdapter);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        List<Fragment> fList;
+        public ScreenSlidePagerAdapter(FragmentManager fm,List<Fragment> fList) {
+            super(fm);
+            this.fList = fList;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fList.size();
+        }
+    }
+
+    WebSocketClient mWebSocketClient;
+
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("ws://websockethost:8080");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                try{
+                    final JSONObject message = new JSONObject(s);
+                    final List<Fragment> fList = new ArrayList<>();
+                    final int status = message.getInt("status");
+                    switch(status) {
+                        case 0:
+                            fList.add(ResponseFragment.newInstance(status,message.getString("prop1")
+                                    ,message.getString("prop2"),message.getString("prop3")
+                                    ,message.getString("prop4"),message.getLong("timeRemaining")
+                                    ,message.getLong("timeTotal")));
+                            aNumber = message.getString("number");
+                            aQuestion = message.getString("question");
+                            break;
+                        case 1:
+                            fList.add(ResponseFragment.newInstance(status));
+                            break;
+                        case 2:
+                            fList.add(ResponseFragment.newInstance(status));
+                            fList.add(new StatFragment());
+                            break;
+                        default:
+                            break;
+                    }
+
+                    final String number = aNumber;
+                    final String question = aQuestion;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (status == 0){
+                                qNumber.setText(number);
+                                qQuestion.setText(question);
+                            }
+                            mPager = (ViewPager) findViewById(R.id.pager);
+                            mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager(), fList);
+                            mPager.setAdapter(mPagerAdapter);
+                        }
+                    });
+                }catch(JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
     }
 }
--- QuizAndroid/app/src/main/java/fr/esigelec/quiz/ResponseFragment.java
+++ QuizAndroid/app/src/main/java/fr/esigelec/quiz/ResponseFragment.java
@@ -1,7 +1,5 @@
 package fr.esigelec.quiz;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,44 +7,40 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ResponseFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ResponseFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ResponseFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_status = "status";
+    private static final String ARG_prop1 = "prop1";
+    private static final String ARG_prop2 = "prop2";
+    private static final String ARG_prop3 = "prop3";
+    private static final String ARG_prop4 = "prop3";
+    private static final String ARG_timeRemaining = "timeRemaining";
+    private static final String ARG_timeTotal = "timeTotal";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int mStatus;
 
-    private OnFragmentInteractionListener mListener;
 
     public ResponseFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ResponseFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ResponseFragment newInstance(String param1, String param2) {
+    public static ResponseFragment newInstance(int status) {
         ResponseFragment fragment = new ResponseFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_status, status);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static ResponseFragment newInstance(int status,String prop1,String prop2,String prop3,String prop4,long time,long timeTotal) {
+        ResponseFragment fragment = new ResponseFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_status, status);
+        args.putString(ARG_prop1, prop1);
+        args.putString(ARG_prop2, prop2);
+        args.putString(ARG_prop3, prop3);
+        args.putString(ARG_prop4, prop4);
+        args.putLong(ARG_timeRemaining, time);
+        args.putLong(ARG_timeTotal, timeTotal);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,8 +49,7 @@ public class ResponseFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mStatus = getArguments().getInt(ARG_status);
         }
     }
 
@@ -67,42 +60,8 @@ public class ResponseFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_response, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
