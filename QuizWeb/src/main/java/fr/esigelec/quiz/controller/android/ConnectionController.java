cac95ab360e37149eab46383b8f7b74874a3afdb
sittingbuzz <picard_simon@orange.fr>
2017-01-06 09:25:55 +0100

    Création fr.esigelec.quiz.controller.android.ConnectionController

--- QuizWeb/src/main/java/fr/esigelec/quiz/controller/android/ConnectionController.java
+++ QuizWeb/src/main/java/fr/esigelec/quiz/controller/android/ConnectionController.java
@@ -1,7 +1,25 @@
 package fr.esigelec.quiz.controller.android;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ConnectionController {
 
-
+	@RequestMapping(value = "/android/", method = RequestMethod.POST)
+	public String connection(@RequestParam("mail") String mail, @RequestParam("mdp") String mdp) {
+		String retourJson = "";
+			
+		//On test si l'utilisateur posséde un compte dans la base de données
+		if(true){
+			
+		} else {
+			
+		}
+		
+		return retourJson;
+	}
 
 }
