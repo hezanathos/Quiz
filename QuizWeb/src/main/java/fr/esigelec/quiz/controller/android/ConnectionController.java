730c8790da797fbfc9f57835ffc6daade1a1b321
sittingbuzz <picard_simon@orange.fr>
2017-01-09 14:58:52 +0100

    Maj de android/ConnectionController et android/InscriptionController avec les nouvelles méthodes de la DAO

--- QuizWeb/src/main/java/fr/esigelec/quiz/controller/android/ConnectionController.java
+++ QuizWeb/src/main/java/fr/esigelec/quiz/controller/android/ConnectionController.java
@@ -1,21 +1,30 @@
 package fr.esigelec.quiz.controller.android;
 
+/**
+ * @author Simon
+ */
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import fr.esigelec.quiz.dao.PersonneDAOImpl;
+import fr.esigelec.quiz.model.Personne;
+
 @Controller
 public class ConnectionController {
 
-	@RequestMapping(value = "/android/connection", method = RequestMethod.POST)
+	@RequestMapping(value = "/android/connexion", method = RequestMethod.POST)
 	public String connection(@RequestParam("email") String email, @RequestParam("password") String password) {
 		String retourJson = "";
-			
-		//TODO mettre a jour avec les méthodes de PersonnesDAO
-		//On test si l'utilisateur posséde un compte dans la base de données
-		if(true){
-			retourJson = "{'status':'OK', 'userId':'userId'}";
+		PersonneDAOImpl pDao = new PersonneDAOImpl();
+		Personne p;
+		
+		//On test si l'utilisateur existe dans la base de données
+		if(pDao.verifPersonne(email, password) != -1){
+			//Si l'utilisateur existe on récupére son id
+			p = pDao.getPersonneByEmail(email);
+			retourJson = "{'status':'OK', 'message':'utilisateur connecté', 'id':" + p.getId() + "}";
 		} else {
 			retourJson = "{'status':'KO', 'message':'utilisateur inconnu'}";
 		}
