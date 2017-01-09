83364a26e81b15bcfca4aba08dba72daac3c9118
Matthieu MUNHOVEN <matthieu.munhoven@groupe-esigelec.org>
2017-01-09 16:28:54 +0100

    Update 2

--- QuizWeb/src/main/java/fr/esigelec/quiz/controller/web/UserController.java
+++ QuizWeb/src/main/java/fr/esigelec/quiz/controller/web/UserController.java
@@ -20,22 +20,41 @@ import java.sql.Timestamp;
 @Controller
 public class UserController {
 	
-	
+	@Autowired
+	private PersonneDAOImpl service;
+
 	@RequestMapping(value = "/inscription", method = RequestMethod.POST)
-	public void inscription(HttpServletRequest request){
-		String nom = request.getParameter("nom");
+	public String inscription(@Valid @ModelAttribute(value="creation") final Personne p,
+							final BindingResult pBindingResult, final ModelMap pModel){
+
+		if (!pBindingResult.hasErrors()) {
+			service.ajouterPersonne(p);
+		}
+		/*String nom = request.getParameter("nom");
 		String motDePasse = request.getParameter("motDePasse");
 		String prenom = request.getParameter("prenom");
 		String courriel = request.getParameter("courriel");
 		Personne p = new Personne(1000,nom,prenom,courriel,motDePasse,0);
 		PersonneDAOImpl pdao = new PersonneDAOImpl();
-		pdao.ajouterPersonne(p);
-		
+		pdao.ajouterPersonne(p);*/
+
+		return "jecpaskoi";
 	}
 	
 	@RequestMapping(value = "/connexion", method = RequestMethod.POST)
-	public void connexion(HttpServletRequest request){
-		String motDePasse = request.getParameter("motDePasse");
+	public String connexion(@Valid @ModelAttribute(value="creation") final String courriel, final String mdp,
+						  final BindingResult pBindingResult, final ModelMap pModel){
+
+		if (!pBindingResult.hasErrors()) {
+			Personne pTemp = service.getPersonne(courriel);
+			if(service.verifPersonne(courriel,mdp)){
+				HttpSession session = request.getSession();
+				session.setAttribute("courriel", pTemp.getMail());
+				session.setAttribute("nom", pTemp.getNom());
+				session.setAttribute("prenom", pTemp.getPrenom());
+			}
+		}
+		/*String motDePasse = request.getParameter("motDePasse");
 		String courriel = request.getParameter("courriel");
 		PersonneDAOImpl pdao = new PersonneDAOImpl();
 		Personne pTemp = pdao.getPersonne(courriel);
@@ -44,13 +63,17 @@ public class UserController {
 			session.setAttribute("courriel", pTemp.getMail());
 			session.setAttribute("nom", pTemp.getNom());
 			session.setAttribute("prenom", pTemp.getPrenom());
-		}
+		}*/
+
+		return "jecpaskoi";
 	}
 
 	@RequestMapping(value = "/deconnexion", method = RequestMethod.GET)
-	public void deconnexion(HttpServletRequest request){
-		HttpSession session = request.getSession();
+	public String deconnexion(final ModelMap pModel){
+		HttpSession session = pModel.getSession();
 		session.invalidate();
+
+		return "jecpaskoi";
 	}
 
 }
