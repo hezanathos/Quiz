3b5208ea3201d75a960d1675e1d6facc0daecd65
Matthieu MUNHOVEN <matthieu.munhoven@groupe-esigelec.org>
2017-01-09 14:36:50 +0100

    Update des controllers

--- QuizWeb/src/main/java/fr/esigelec/quiz/controller/web/QuizController.java
+++ QuizWeb/src/main/java/fr/esigelec/quiz/controller/web/QuizController.java
@@ -3,6 +3,12 @@ package fr.esigelec.quiz.controller.web;
 import org.springframework.stereotype.Controller;
 
 import fr.esigelec.quiz.model.Quiz;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.sql.Timestamp;
 
 /**
  * Created by Edouard on 04/01/2017.
@@ -16,5 +22,15 @@ public class QuizController {
 	public void startQuiz(int idQuiz){
 		
 	}
-	
+
+	@RequestMapping(value = "/ajouterQuiz", method = RequestMethod.POST)
+	public void ajouterQuiz(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		String courriel = (String) session.getAttribute("courriel");
+		String libelle = request.getParameter("libelle");
+		Timestamp dateDebutQuiz = (Timestamp) request.getParameter("dateDebutQuiz");
+		int noQuestionCourant = (Timestamp) request.getParameter("noQuestionCourant");
+
+		Quiz quiz = new Quiz(libelle, dateDebutQuiz, noQuestionCourant, etape, dateDebutQuestion, listeQuestion);
+	}
 }
