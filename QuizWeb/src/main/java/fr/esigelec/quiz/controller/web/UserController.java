cc1200ddfd02f6f0c122de6b12c0e4ff861f6738
guillaumesauvage <guillaumesauv@gmail.com>
2017-01-11 15:52:38 +0100

    redone

--- QuizWeb/src/main/java/fr/esigelec/quiz/controller/web/UserController.java
+++ QuizWeb/src/main/java/fr/esigelec/quiz/controller/web/UserController.java
@@ -1,12 +1,11 @@
 package fr.esigelec.quiz.controller.web;
-import fr.esigelec.quiz.model.*;
 
+import fr.esigelec.quiz.model.*;
 
 import fr.esigelec.quiz.dao.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< master
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -21,10 +20,10 @@ import javax.servlet.http.*;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private PersonneDAOImpl service;
-	
+
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String accueilConnexion(Model model) {
 		Personne p = new Personne();
@@ -33,7 +32,7 @@ public class UserController {
 		model.addAttribute("formulaireConnexion", p);
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "/inscription", method = RequestMethod.GET)
 	public String accueilInscription(Model model) {
 		Personne p = new Personne();
@@ -44,132 +43,57 @@ public class UserController {
 		model.addAttribute("formulaireInscription", p);
 		return "inscription";
 	}
-	
+
 	@RequestMapping(value = "/inscription.do", method = RequestMethod.POST)
 	public String inscription(@RequestParam("mail") String mail, @RequestParam("mdp") String mdp,
-			@RequestParam("nom") String nom, @RequestParam("prenom") String prenom,Model model){
-		
+			@RequestParam("nom") String nom, @RequestParam("prenom") String prenom, Model model) {
+
 		Personne p = new Personne();
 		p.setNom(nom);
 		p.setPrenom(prenom);
 		p.setMail(mail);
 		p.setMdp(mdp);
 		p.setDroits(0);
-		
+
 		if (service.ajouterPersonne(p) == 1) {
-			
+
 			return "redirect:/index";
-		}
-		else if(service.ajouterPersonne(p) == -1)
-		{
-			model.addAttribute("erreurPersonneExiste","Email déjà utilisé");
+		} else if (service.ajouterPersonne(p) == -1) {
+			model.addAttribute("erreurPersonneExiste", "Email déjà utilisé");
 			return "inscription";
 		}
-		/*String nom = request.getParameter("nom");
-		String motDePasse = request.getParameter("motDePasse");
-		String prenom = request.getParameter("prenom");
-		String courriel = request.getParameter("courriel");
-		Personne p = new Personne(1000,nom,prenom,courriel,motDePasse,0);
-		PersonneDAOImpl pdao = new PersonneDAOImpl();
-		pdao.ajouterPersonne(p);*/
+		/*
+		 * String nom = request.getParameter("nom"); String motDePasse =
+		 * request.getParameter("motDePasse"); String prenom =
+		 * request.getParameter("prenom"); String courriel =
+		 * request.getParameter("courriel"); Personne p = new
+		 * Personne(1000,nom,prenom,courriel,motDePasse,0); PersonneDAOImpl pdao
+		 * = new PersonneDAOImpl(); pdao.ajouterPersonne(p);
+		 */
 		return "inscription";
 
 	}
-	
-	@RequestMapping(value = "/connexion.do", method = RequestMethod.POST)
-	public String connexion(@RequestParam("mail") String mail, @RequestParam("mdp") String mdp,Model model){
 
-		
+	@RequestMapping(value = "/connexion.do", method = RequestMethod.POST)
+	public String connexion(@RequestParam("mail") String mail, @RequestParam("mdp") String mdp, Model model) {
+
 		Personne pTemp = service.getPersonneByEmail(mail);
-		if(service.verifPersonne(mail,mdp) != (-1)){//On verifie si la personne existe
-			
+		if (service.verifPersonne(mail, mdp) != (-1)) {// On verifie si la
+														// personne existe
+
 			return "redirect:/quiz";
-		}
-		else
-		{
+		} else {
 			model.addAttribute("erreurUtilisateurInconnu", "Utilisateur inconnu");
 			return "index";
 		}
-	/*String motDePasse = request.getParameter("motDePasse");
-	String courriel = request.getParameter("courriel");
-	PersonneDAOImpl pdao = new PersonneDAOImpl();
-	Personne pTemp = pdao.getPersonne(courriel);
-	if(pdao.verifPersonne(courriel,motDePasse)){
-		HttpSession session = request.getSession();
-		session.setAttribute("courriel", pTemp.getMail());
-		session.setAttribute("nom", pTemp.getNom());
-		session.setAttribute("prenom", pTemp.getPrenom());
-	}*/
-		
-=======
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import javax.servlet.http.*;
-
-/**
- * Created by Edouard on 04/01/2017.
- *
- * @author - Edouard PETIT Matthieu Munhoven
- */
-
-@Controller
-public class UserController {
-	
-	@Autowired
-	private PersonneDAOImpl service;
-
-	@RequestMapping(value = "/inscription", method = RequestMethod.POST)
-	public String inscription( @ModelAttribute(value="creation") final Personne p,
-							   final ModelMap pModel){
-
-		
-		/*String nom = request.getParameter("nom");
-		String motDePasse = request.getParameter("motDePasse");
-		String prenom = request.getParameter("prenom");
-		String courriel = request.getParameter("courriel");
-		Personne p = new Personne(1000,nom,prenom,courriel,motDePasse,0);
-		PersonneDAOImpl pdao = new PersonneDAOImpl();
-		pdao.ajouterPersonne(p);*/
-
-		return "jecpaskoi";
+		/*
+		 * String motDePasse = request.getParameter("motDePasse"); String
+		 * courriel = request.getParameter("courriel"); PersonneDAOImpl pdao =
+		 * new PersonneDAOImpl(); Personne pTemp = pdao.getPersonne(courriel);
+		 * if(pdao.verifPersonne(courriel,motDePasse)){ HttpSession session =
+		 * request.getSession(); session.setAttribute("courriel",
+		 * pTemp.getMail()); session.setAttribute("nom", pTemp.getNom());
+		 * session.setAttribute("prenom", pTemp.getPrenom()); }
+		 */
 	}
-	
-	@RequestMapping(value = "/connexion", method = RequestMethod.POST)
-	public String connexion( @ModelAttribute(value="connexion") final String courriel, final String mdp,
-						    final ModelMap pModel, HttpServletRequest request){
-
-	
-			Personne pTemp = service.getPersonneByEmail(courriel);
-			if(service.verifPersonne(courriel,mdp)!=(-1)){
-				HttpSession session = request.getSession();
-				session.setAttribute("courriel", pTemp.getMail());
-				session.setAttribute("nom", pTemp.getNom());
-				session.setAttribute("prenom", pTemp.getPrenom());
-			}
-		
-		/*String motDePasse = request.getParameter("motDePasse");
-		String courriel = request.getParameter("courriel");
-		PersonneDAOImpl pdao = new PersonneDAOImpl();
-		Personne pTemp = pdao.getPersonne(courriel);
-		if(pdao.verifPersonne(courriel,motDePasse)){
-			HttpSession session = request.getSession();
-			session.setAttribute("courriel", pTemp.getMail());
-			session.setAttribute("nom", pTemp.getNom());
-			session.setAttribute("prenom", pTemp.getPrenom());
-		}*/
-
-		return "jecpaskoi";
-
-	}
-
-
-	@RequestMapping(value = "/deconnexion", method = RequestMethod.GET)
-	public String deconnexion(HttpServletRequest request){
-		HttpSession session = request.getSession();
-		session.invalidate();
-
-		return "jecpaskoi";
-	}
-
 }
