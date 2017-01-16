379ccafeb019f4e4e202afb451cca8356c45734c
alexLecoq <a.lecoq.14@groupe-esigelec.fr>
2017-01-16 13:51:56 +0100

    implementing

--- QuizWeb/src/main/java/fr/esigelec/quiz/dao/QuizDAOImpl.java
+++ QuizWeb/src/main/java/fr/esigelec/quiz/dao/QuizDAOImpl.java
@@ -8,60 +8,67 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import fr.esigelec.quiz.model.Question;
 import fr.esigelec.quiz.model.Quiz;
 
 /**
  * 
  * @author Sahobau
  * 
- * */
+ */
 
-@Repository("quizDAOImpl") 
+@Repository("quizDAOImpl")
 @Transactional(propagation = Propagation.SUPPORTS)
 public class QuizDAOImpl implements QuizDAO {
-	
-	 @Autowired
-	 private SessionFactory sessionFactory;
+
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	@Override
 	public void ajouterQuiz(Quiz quiz) {
-		   sessionFactory.getCurrentSession().saveOrUpdate(quiz);
-		
+		sessionFactory.getCurrentSession().saveOrUpdate(quiz);
+
 	}
 
 	@Override
 	@SuppressWarnings("unchecked")
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 
 	public List<Quiz> getListeQuizzes() {
-		
-		/*Session session = this.sessionFactory.getCurrentSession();
-		List<Quiz> quizzesList = session.createQuery("from quiz").list();
-		
-		return quizzesList;*/
-		
+
+		/*
+		 * Session session = this.sessionFactory.getCurrentSession(); List<Quiz>
+		 * quizzesList = session.createQuery("from quiz").list();
+		 * 
+		 * return quizzesList;
+		 */
+
 		return (List<Quiz>) sessionFactory.getCurrentSession().createCriteria(Quiz.class).list();
 
 	}
 
 	@Override
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	public Quiz getQuiz(int id) {
-		
-				  return (Quiz) sessionFactory.getCurrentSession().get(Quiz.class, id);
+
+		return (Quiz) sessionFactory.getCurrentSession().get(Quiz.class, id);
 	}
-	
-/*
+
 	@Override
-	public void supprimerQuiz(int id) {
-		
-		Session session = this.sessionFactory.getCurrentSession();
-		Quiz q = (Quiz) session.load(Quiz.class, new Integer(id));
-		if(null != q){
-			session.delete(q);
-		}
-		
+	public Question getCurrentQuestion(int idQuiz) {
+		Quiz qz = getQuiz(idQuiz);
+
+		return qz.getListeQuestion().get(qz.getNoQuestionCourant());
 	}
-	*/
+
+	/*
+	 * @Override public void supprimerQuiz(int id) {
+	 * 
+	 * Session session = this.sessionFactory.getCurrentSession(); Quiz q =
+	 * (Quiz) session.load(Quiz.class, new Integer(id)); if(null != q){
+	 * session.delete(q); }
+	 * 
+	 * }
+	 */
 
 }
