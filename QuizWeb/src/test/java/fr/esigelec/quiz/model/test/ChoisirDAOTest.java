4cc55efbc9bb91f805271672929b153598772f91
Saobane <Saobane@10.20.48.68>
2017-01-16 16:01:23 +0100

    test getBonnereponses

--- QuizWeb/src/test/java/fr/esigelec/quiz/model/test/ChoisirDAOTest.java
+++ QuizWeb/src/test/java/fr/esigelec/quiz/model/test/ChoisirDAOTest.java
@@ -97,43 +97,46 @@ public class ChoisirDAOTest {
 	public void testgetNbBonnesReponses(){
 		ApplicationContext context = new ClassPathXmlApplicationContext("dispatcher-servlet.xml");
 
-		Timestamp timestampDate1 = Timestamp.valueOf("2007-09-23 10:10:10.0");
-		Timestamp timestampDate2 = Timestamp.valueOf("2007-09-23 12:10:10.0");
+		Timestamp timestampDate1 = Timestamp.valueOf("2017-01-16 10:10:10.0");
+		Timestamp timestampDate2 = Timestamp.valueOf("2017-01-16 12:10:10.0");
 		
 		List<Question> listeQuestion1 = new ArrayList<Question>();
 		
-		listeQuestion1.add(new Question("libelle1Q1"));
-		listeQuestion1.add(new Question("libelle1Q2"));
+		listeQuestion1.add(new Question(100,"libelle1Q1"));
+		listeQuestion1.add(new Question(101,"libelle1Q2"));
 		
-		List<Question> listeQuestion2 = new ArrayList<Question>();
+		
+		
+		/*List<Question> listeQuestion2 = new ArrayList<Question>();
 		
 		listeQuestion2.add(new Question("libelle2Q1"));
-		listeQuestion2.add(new Question("libelle2Q2"));
+		listeQuestion2.add(new Question("libelle2Q2"));*/
 		
 		Personne p1= new Personne ("LESIEU","Rock","lesieurock@der.fr","mdp1",1000);
-		Personne p2= new Personne ("ROUX","Richard","rouxrichard@der.fr","mdp2",0);
+		Personne p2= new Personne (1001,"Richard","richard","rouxrichard@der.fr","mdp2",0);
 		
 		PersonneDAO personneDAO= (PersonneDAO) context.getBean("personneDAOImpl");
 		personneDAO.ajouterPersonne(p1);
 		personneDAO.ajouterPersonne(p2);
+		
 		p1=personneDAO.getPersonneByEmail("lesieurock@der.fr");
-		p2=personneDAO.getPersonneByEmail("rouxrichard@der.fr");
+		p2=personneDAO.getPersonne(1001);
 		
 		
 
 		
-		Quiz quiz1 = new Quiz("libelleQuiz1",1,Timestamp.valueOf("2007-09-23 09:10:10.0"),1,1,Timestamp.valueOf("2007-09-23 09:16:10.0"),listeQuestion1);
-		Quiz quiz2 = new Quiz("libelleQuiz2",1,Timestamp.valueOf("2007-09-23 10:10:10.0"),1,1,Timestamp.valueOf("2007-09-23 10:16:10.0"),listeQuestion2);
+		Quiz quiz1 = new Quiz(100,"libelleQuiz1-1",1,Timestamp.valueOf("2017-01-16 10:10:10.0"),1,1,Timestamp.valueOf("2017-01-16 10:16:10.0"),listeQuestion1);
+		//Quiz quiz2 = new Quiz("libelleQuiz2",1,Timestamp.valueOf("2007-09-23 10:10:10.0"),1,1,Timestamp.valueOf("2007-09-23 10:16:10.0"),listeQuestion2);
 
 		QuizDAO quizDAO=(QuizDAO) context.getBean("quizDAOImpl");
 		quizDAO.ajouterQuiz(quiz1);
-		quizDAO.ajouterQuiz(quiz2);
+		//quizDAO.ajouterQuiz(quiz2);
 		
 		
 		
 		
-		Proposition prop1 = new Proposition("libelleprop1", 1, listeQuestion1.get(0));
-		Proposition prop2 = new Proposition("libelleprop2", 1, listeQuestion2.get(0));
+		Proposition prop1 = new Proposition(100,"libelleprop1", 1, listeQuestion1.get(0));
+		Proposition prop2 = new Proposition(101,"libelleprop2", 1, listeQuestion1.get(0));
 		
 		PropositionDAO propositionDAO= (PropositionDAO) context.getBean("propositionDAOImpl");
 		propositionDAO.ajouterProposition(prop1);
@@ -141,7 +144,7 @@ public class ChoisirDAOTest {
 
 		
 		Choisir chx1= new Choisir(timestampDate1,p1,quiz1,prop1);
-		Choisir chx2= new Choisir(timestampDate2,p2,quiz2,prop2);
+		Choisir chx2= new Choisir(timestampDate2,p2,quiz1,prop2);
 		
 		ChoisirDAO cdao = (ChoisirDAO) context.getBean("choisirDAOImpl");
 		
@@ -149,10 +152,12 @@ public class ChoisirDAOTest {
 		cdao.ajouterChoix(chx2);
 		
 		
-		int nbbonnesRp=cdao.getNbBonnesReponses(quiz1.getId(), listeQuestion1.get(0).getId());
+		int nbbonnesRp=cdao.getNbBonnesReponses(100,100);
 		
-		if(nbbonnesRp==1){
-			assertEquals(nbbonnesRp,1);
+		System.out.println(nbbonnesRp);
+		
+		if(nbbonnesRp==2){
+			assertEquals(nbbonnesRp,2);
 			
 		}else{
 			fail("Test not good"+nbbonnesRp);
